1689eb4c517662db8bd29757ed354472fee4af63
Yan Ponomarev <yanponomarev@gmail.com>
2022-07-27 19:38:04 +0300

    Создали первую версию таблиц
    Всё

--- src/db/models/__init__.py
+++ src/db/models/__init__.py
@@ -1,1 +1,2 @@
+from . import group_emails
 from . import single_emails
--- src/db/models/group_emails.py
+++ src/db/models/group_emails.py
@@ -3,24 +3,22 @@
 """
 from uuid import uuid4
 
-from sqlalchemy import Table, Column, Integer, DateTime, func, String
+from sqlalchemy import Table, Column, Integer, DateTime, func
 from sqlalchemy.dialects.postgresql import UUID, JSON
 
 from db.db_init import Base
 
-single_emails = Table(
-    'single_emails',
+group_emails = Table(
+    'group_emails',
     Base.metadata,
     Column('id', UUID(as_uuid=True), primary_key=True, default=uuid4, unique=True, nullable=False),
     Column('destination_id', UUID(as_uuid=True), nullable=False),
     Column('template_id', UUID(as_uuid=True), nullable=False),
-    Column('group_id', UUID(as_uuid=True)),
     Column('message', JSON, nullable=False),
     Column('delay', Integer, default=0, nullable=False),
     Column('created_at', DateTime(timezone=True), default=func.now()),
     Column('updated_at', DateTime(timezone=True), onupdate=func.now()),
     Column('deleted_at', DateTime(timezone=True)),
-    Column('sent_at', DateTime(timezone=True)),
-    Column('sent_result', String),
+    Column('passed_to_handler', DateTime(timezone=True)),
     schema='email'
 )
