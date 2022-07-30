a9f0f6cf73646d520eda5646719fe06f0c1631c2
Yan Ponomarev <yanponomarev@gmail.com>
2022-07-30 21:29:40 +0300

    Создали абстрактный класс AbstractDBClient
    Всё.

--- src/db/storage/orm_factory.py
+++ src/db/storage/orm_factory.py
@@ -1,10 +1,12 @@
+"""Модуль содержит классы для асинхронной работы с БД."""
 import databases
 
 from config.settings import config
+from db.storage.abstract_classes import AbstractDBClient
 
 
-class AsyncPGClient:
-
+class AsyncPGClient(AbstractDBClient):
+    """Класс создаёт сессию для асинхронной работы с Postgres."""
     def __init__(self):
         user = config.pg.login.get_secret_value()
         password = config.pg.password.get_secret_value()
@@ -13,9 +15,11 @@ class AsyncPGClient:
         self.session = databases.Database(f'postgresql://{user}:{password}@{host}/{db_name}')
 
     async def start(self):
+        """Метод создаёт соединение с БД."""
         await self.session.connect()
 
     async def stop(self):
+        """Метод закрывает соединение с БД."""
         await self.session.disconnect()
 
 
