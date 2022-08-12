ffe0005cf2f63dd6e853355f3007700082d1df53
Vlada Sabelnikova <vladasabelnikova@gmail.com>
2022-08-12 19:05:32 +0300

    Всё работает!
    Победа! Перемога!

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,16 +1,31 @@
+"""Модуль содержит настройки приложения."""
 from pydantic import BaseSettings, SecretStr
 
 from security.vault_app_config import vault
 
 
+class MessageRabbitSettings(BaseSettings):
+
+    """Настройки сообщений для брокера."""
+
+    expiration: int = 30_000  # 30 минут. Указываем миллисекунды.
+
+
 class RabbitSettings(BaseSettings):
+
+    """Настройки RabbitMQ."""
+
     host: str = vault.get_secret('rabbit_host')
     port: int = vault.get_secret('rabbit_port')
     login: SecretStr = vault.get_secret('rabbit_login')
     password: SecretStr = vault.get_secret('rabbit_password')
+    message: MessageRabbitSettings = MessageRabbitSettings()
 
 
 class Config(BaseSettings):
+
+    """Настройки приложения."""
+
     rabbit_mq: RabbitSettings = RabbitSettings()
 
 
--- src/db/message_brokers/rabbit_message_broker.py
+++ src/db/message_brokers/rabbit_message_broker.py
@@ -1,22 +1,36 @@
+"""Модуль содержит класс с брокером сообщений."""
 import asyncio
 from typing import Optional
 
 import aio_pika
 from aio_pika import Message, DeliveryMode, ExchangeType
-from aio_pika.abc import AbstractRobustConnection
+from aio_pika.abc import AbstractRobustConnection, AbstractQueueIterator
 
 from config.settings import config
+from db.message_brokers.abstract_classes import AbstractMessageBroker
 
 
-class RabbitMessageBroker:
+class RabbitMessageBroker(AbstractMessageBroker):
 
-    def __init__(self):
+    """Класс с интерфейсом брокера сообщений RabbitMQ."""
+
+    def __init__(self) -> None:
+        """Конструктор."""
         self.host = config.rabbit_mq.host
         self.port = config.rabbit_mq.port
         self.login = config.rabbit_mq.login
         self.password = config.rabbit_mq.password
 
-    async def get(self, queue_name):
+    async def get(self, queue_name: str) -> AbstractQueueIterator:
+        """
+        Метод достаёт сообщения (возвращает итерируемый объект) из очереди с названием queue_name.
+
+        Args:
+            queue_name: название очереди, из которой хотим получить данные
+
+        Returns:
+            Вернёт асинхронный итератор.
+        """
         connection = await self._get_connect()
         channel = await connection.channel()
         queue = await channel.declare_queue(name=queue_name, durable=True)
@@ -27,11 +41,21 @@ class RabbitMessageBroker:
     async def put(
         self,
         message_body: bytes,
-        expiration: int,
         queue_name: str,
         exchange_name: str,
-        message_headers: Optional[dict] = None
-    ):
+        message_headers: Optional[dict] = None,
+        expiration: int = config.rabbit_mq.message.expiration
+    ) -> None:
+        """
+        Метод складывает сообщение в очередь.
+
+        Args:
+            message_body: содержимое сообщения
+            expiration: ttl сообщения, указывается в миллисекундах
+            queue_name: название очереди, в которую нужно отправить сообщение
+            exchange_name: название обменника
+            message_headers: заголовок сообщения (сюда нужно вставить x-request-id)
+        """
         connection = await self._get_connect()
         channel = await connection.channel()
         exchange = await channel.declare_exchange(name=exchange_name, type=ExchangeType.FANOUT)
@@ -42,31 +66,44 @@ class RabbitMessageBroker:
             expiration=expiration
         )
 
-        await exchange.publish(message, routing_key=queue_name)
+        await exchange.publish(message=message, routing_key=queue_name)
+        await connection.close()
 
     async def _get_connect(self) -> AbstractRobustConnection:
-        connection = await aio_pika.connect_robust(
+        """
+        Внутренний метод класса (нужен для создания соединения).
+
+        Returns:
+            Вернёт соединение с БД.
+        """
+        return await aio_pika.connect_robust(
             host=self.host,
             port=self.port,
             login=self.login.get_secret_value(),
             password=self.password.get_secret_value()
         )
-        return connection
 
 
-message_broker_factory = RabbitMessageBroker()
+message_broker_factory: AbstractMessageBroker = RabbitMessageBroker()
 
 
-async def callback():
+async def callback() -> None:
+    """
+    Функция демонстрирует возможности класса.
+    В данном случае демонстрация работы функции по выкусу и обработке данных.
+    """
     data_from_consumer = await message_broker_factory.get('queue_1')
     async for message in data_from_consumer:
-        print(message.body.decode(), message.info()['delivery_tag'], message.info()['redelivered'])
+        print('выкусили', message.body.decode())  # noqa: WPS421
         await asyncio.sleep(3)
         await message.ack()
 
 
-async def create_data():
-
+async def create_data() -> None:
+    """
+    Функция демонстрирует возможности класса.
+    В данном случае демонстрация работы функции по вставке данных.
+    """
     expiration = 5 * 1000  # 5 минут.
     queue_name = 'queue_1'
     exchange_name = 'exc_queue_1'
@@ -79,15 +116,16 @@ async def create_data():
             queue_name=queue_name,
             exchange_name=exchange_name
         )
-        print('записали', message_body)
+        print('записали', message_body)  # noqa: WPS421
 
 
-async def main():
+async def main() -> None:
+    """Две функции-демонстратора в одном."""
     await create_data()
     await callback()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
     loop.close()
