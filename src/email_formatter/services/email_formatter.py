1abad766daaca1dece9b31575ae9e82836adc75f
Vlada Sabelnikova <vladasabelnikova@gmail.com>
2022-08-20 19:03:22 +0300

    Пока не работает ничего. Опорная точка
    Всё... (databases отказывается работать)

--- src/email_formatter/services/email_formatter.py
+++ src/email_formatter/services/email_formatter.py
@@ -1,14 +1,14 @@
-from email_formatter.services.auth import auth_service
-from email_formatter.services.pg import pg_service
+# from email_formatter.services.auth import auth_service
+from email_formatter.services.pg import db_service
 
 
 class EmailFormatterService:
 
     async def get_data(self, notification_id):
-        template_id, destination_id, message = pg_service.get_raw_data_by_id(notification_id=notification_id)
-        template = pg_service.get_template_by_id(template_id=template_id)
-        email = auth_service.get_email_by_id(email_id=destination_id)
-        return message, template, email
+        template_id, destination_id, message = await db_service.get_raw_data_by_id(notification_id=notification_id)
+        template = await db_service.get_template_by_id(template_id=template_id)
+        # email = await auth_service.get_email_by_id(email_id=destination_id)
+        return message, template  # , email
 
     async def render_html(self):
         ...
@@ -21,3 +21,6 @@ class EmailFormatterService:
 
     async def start(self):
         ...
+
+
+email_formatter_service = EmailFormatterService()
--- src/email_formatter/services/pg.py
+++ src/email_formatter/services/pg.py
@@ -2,10 +2,14 @@ from sqlalchemy import select, not_, and_
 
 from db.models.email_single_notifications import SingleEmails
 from db.models.email_templates import HTMLTemplates
+from db.storage.abstract_classes import AbstractDBClient
 from db.storage.orm_factory import db
 
 
-class PGService:
+class DBService:
+
+    def __init__(self, database: AbstractDBClient):
+        self.db = database
 
     async def get_raw_data_by_id(self, notification_id):
         query = select(
@@ -13,19 +17,20 @@ class PGService:
             SingleEmails.destination_id,
             SingleEmails.message
         ).where(
-            and_(
-                SingleEmails.deleted_at is None,
+            # and_(
+                # SingleEmails.deleted_at is None,
                 SingleEmails.id == notification_id
-            )
+            # )
         )
-        return db.execute(query)
+        await self.db.start()
+        return await self.db.execute(query)
 
     async def get_template_by_id(self, template_id):
         query = select(HTMLTemplates.template).where(HTMLTemplates.id == template_id)
-        return db.execute(query)
+        return await self.db.execute(query)
 
     async def put_passed_to_handled_at(self):
         ...
 
 
-pg_service = PGService()
+db_service = DBService(database=db)
