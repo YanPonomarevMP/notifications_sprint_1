8ec7be5f7e8ba097a90e6a242aa8a5ba00fb3ef7
Vlada Sabelnikova <vladasabelnikova@gmail.com>
2022-08-23 00:18:25 +0300

    Получилось избежать parse_obj_as
    всё

--- src/email_formatter/services/pg.py
+++ src/email_formatter/services/pg.py
@@ -1,7 +1,8 @@
+from datetime import datetime
 from uuid import UUID
 
 from pydantic import parse_obj_as
-from sqlalchemy import select, and_
+from sqlalchemy import select, and_, update, func
 
 from db.models.email_single_notifications import SingleEmails
 from db.models.email_templates import HTMLTemplates
@@ -17,23 +18,23 @@ class DBService:
 
     async def get_raw_data_by_id(self, notification_id: UUID) -> RawDataModel:
         query = self._get_query_select_raw_data(notification_id)
-
         async with self.db:
-            result = await self.db.execute(query)
+            result, = await self.db.execute(query)
 
         # Результат можно, конечно, и в ручную перебрать, но выйдет громоздко.
-        return parse_obj_as(RawDataModel, result[0]._mapping)
+        return RawDataModel(**result._mapping)
 
     async def get_template_by_id(self, template_id: UUID) -> TemplateModel:
         query = self._get_query_select_template_by_id(template_id)
-
         async with self.db:
-            result = await self.db.execute(query)
+            result, = await self.db.execute(query)
 
         return parse_obj_as(TemplateModel, result[0]._mapping)  # TODO: А нужна ли тут модель вообще?
 
-    async def put_passed_to_handled_at(self):
-        ...
+    async def put_passed_to_handled_at(self, notification_id):
+        query = self._get_query_update_passed_to_handled_at(notification_id)
+        async with self.db:
+            await self.db.execute(query)
 
     def _get_query_select_raw_data(self, notification_id):
         return select(
@@ -49,6 +50,15 @@ class DBService:
 
     def _get_query_select_template_by_id(self, template_id):
         return select(HTMLTemplates.template).filter(HTMLTemplates.id == template_id)
+
+    def _get_query_update_passed_to_handled_at(self, notification_id):
+        return update(
+            SingleEmails
+        ).filter(
+            SingleEmails.id == notification_id
+        ).values(
+            passed_to_handled_at=func.now()
+        )
 
 
 db_service = DBService(database=db)
