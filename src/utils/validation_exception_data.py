c7392efc810f333d647872f576ea09bdd063a2a0
Yan Ponomarev <yanponomarev@gmail.com>
2022-08-24 20:10:31 +0300

    Модуль validation_exception_data.py исправлен
    Всё.

--- src/utils/validation_exception_data.py
+++ src/utils/validation_exception_data.py
@@ -1,8 +1,9 @@
 """Модуль содержит функцию для получения данных из RequestValidationError."""
 
 from fastapi.exceptions import RequestValidationError
+from pydantic import ValidationError
 
-from notifier_api.models.access_log import RequestValidationErrorData
+from notifier_api.models.access_log import RequestValidationErrorData, PydanticValidationErrorData
 
 
 async def get_exception_data(exception: RequestValidationError) -> RequestValidationErrorData:
@@ -19,6 +20,24 @@ async def get_exception_data(exception: RequestValidationError) -> RequestValida
     exc.detail = exception.errors()[0]  # type: ignore
     exc.source = exception.errors()[0]['loc'][0]  # type: ignore
     exc.field = exception.errors()[0]['loc'][1]  # type: ignore
+    exc.msg = exception.errors()[0]['msg']  # type: ignore
+
+    return exc
+
+
+def parse_pydantic_validation_exception(exception: ValidationError) -> PydanticValidationErrorData:
+    """
+    Функция парсит ValidationError pydantic.
+
+    Args:
+        exception: исключение ValidationError
+
+    Returns:
+        Вернёт pydantic модель c данными.
+    """
+    exc = PydanticValidationErrorData()
+    exc.detail = exception.errors()[0]  # type: ignore
+    exc.source = exception.errors()[0]['loc'][0]  # type: ignore
     exc.msg = exception.errors()[0]['msg']  # type: ignore
 
     return exc
