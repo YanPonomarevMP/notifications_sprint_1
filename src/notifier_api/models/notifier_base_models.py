cff3676fa9925318c980f06e41cc28b128d7990d
Yan Ponomarev <yanponomarev@gmail.com>
2022-08-27 01:27:24 +0300

    Выделил базовый класс с моделями notifier_base_models.py
    Всё

---
+++ src/notifier_api/models/notifier_base_models.py
@@ -0,0 +1,16 @@
+"""Модель содержит общие pydantic модели для всех ручек API."""
+from typing import Optional
+from uuid import UUID
+
+from notifier_api.models.base_orjson import BaseOrjson
+
+
+class IdempotencyKeyChecker(BaseOrjson):
+
+    """
+    Модель для проверки корректности UUID.
+    В случае ошибки она вызовет ValidationError,
+    которое будет перехвачено валидатором основной модели
+    """
+
+    idempotency_key: Optional[UUID]
--- src/notifier_api/models/notifier_html_template.py
+++ src/notifier_api/models/notifier_html_template.py
@@ -8,18 +8,8 @@ from models.base_orjson import BaseOrjson  # type: ignore
 from pydantic import validator, ValidationError
 
 from notifier_api.models.http_responses import http  # type: ignore
+from notifier_api.models.notifier_base_models import IdempotencyKeyChecker
 from utils.validation_exception_data import parse_pydantic_validation_exception
-
-
-class IdempotencyKeyChecker(BaseOrjson):
-
-    """
-    Модель для проверки корректности UUID.
-    В случае ошибки она вызовет ValidationError,
-    которое будет перехвачено валидатором основной модели
-    """
-
-    idempotency_key: Optional[UUID]
 
 
 class HtmlTemplatesRequest(BaseOrjson):
@@ -46,7 +36,7 @@ class HtmlTemplatesQuery(BaseOrjson):
         validate_assignment = True
 
     @validator('id', pre=True)
-    def encode_to_bytes(cls, filed_value: str) -> UUID:  # noqa: WPS110, N805
+    def encode_to_bytes(cls, filed_value: UUID) -> UUID:  # noqa: WPS110, N805
         """
         Метод проверяет корректность UUID и вызывает HTTPException вместо ValidationError.
         Это позволяет отправить клиенту 400 вместо 500 и записать ошибку в лог.
