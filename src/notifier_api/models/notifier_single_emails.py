35301bfa6cc65571148a98e00845b0c7f957557d
Yan Ponomarev <yanponomarev@gmail.com>
2022-08-27 04:51:41 +0300

    Рефактор 5
    Всё

--- src/notifier_api/models/notifier_single_emails.py
+++ src/notifier_api/models/notifier_single_emails.py
@@ -3,13 +3,9 @@ from datetime import datetime
 from typing import Optional, Union, List, Dict
 from uuid import UUID
 
-from fastapi import HTTPException
 from models.base_orjson import BaseOrjson  # type: ignore
-from pydantic import validator, ValidationError
 
 from notifier_api.models.http_responses import http  # type: ignore
-from notifier_api.models.notifier_base_models import IdempotencyKeyChecker
-from utils.validation_exception_data import parse_pydantic_validation_exception
 
 
 class SingleEmailsRequest(BaseOrjson):
@@ -55,34 +51,6 @@ class SingleEmailsQuery(BaseOrjson):
         """Настройка валидации при изменении значения поля."""
 
         validate_assignment = True
-
-    @validator('id', pre=True)
-    def encode_to_bytes(cls, filed_value: UUID) -> UUID:  # noqa: WPS110, N805
-        """
-        Метод проверяет корректность UUID и вызывает HTTPException вместо ValidationError.
-        Это позволяет отправить клиенту 400 вместо 500 и записать ошибку в лог.
-
-        Args:
-            filed_value: значение, которое нужно проверить
-
-        Returns:
-            Вернёт UUID.
-
-        Raises:
-            HTTPException:
-                В случае некорректного ID вызовет HTTPException.
-        """
-        try:
-            good_uuid = IdempotencyKeyChecker(idempotency_key=filed_value)
-
-        except ValidationError as exception:
-            err = parse_pydantic_validation_exception(exception)
-
-            raise HTTPException(
-                status_code=http.request_validation_error.code,
-                detail=f'{err.msg} - {err.source}'
-            )
-        return good_uuid.idempotency_key
 
 
 class SingleEmailsResponse(BaseOrjson):
